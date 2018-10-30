2e7880e9d7807c408ba59d5552025d9f8ece5298
Kukikoe <ja.bondareva2014@yandex.ru>
2018-10-30 21:27:39 +0200

    add todos in localStorage and can add todos in list on the page

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,30 @@
-let addBtn = document.getElementById("add-btn");
-let addTask = document.getElementById("add-task");
-let boolValClick = false;
-addBtn.addEventListener("click", getBlockAddTask);
+const addBtn = document.getElementById("add-btn");
+const addTask = document.getElementById("add-task");
+const addTaskBtn = document.getElementById("add-task-btn");
 
+//const deleteTodoBtn = document.querySelector("icon-delete");
+let taskInput = document.querySelector(".add-task-input");
+
+
+let newTodo;
+let arr = [];
+let boolValClick = false;
+
+addBtn.addEventListener("click", getBlockAddTask);
+addTaskBtn.addEventListener("click", function() {
+	let arrayTodos = JSON.parse(localStorage.getItem("ArrayTodos"));
+	console.log(arrayTodos.length)
+	let shit = document.querySelector("#todo-template").children[0];
+	let cln = shit.cloneNode(true);
+	addTaskInList(cln);
+});
+
+/*deleteTodoBtn.addEventListener("click", deleteTodo);
+
+function deleteTodo() {
+	console.log("hef")
+}
+*/
 function getBlockAddTask() {
 	if (boolValClick) {
 		addTask.classList.remove("active");
@@ -13,3 +35,10 @@ function getBlockAddTask() {
 		boolValClick = true;
 	}
 }
+
+function addTaskInList(cln) {
+	cln.querySelector(".todo__title").innerHTML = taskInput.value;
+	arr.push(cln.querySelector(".todo__title").innerHTML);
+	document.getElementById("todo-list").appendChild(cln);
+	localStorage.setItem("ArrayTodos",  JSON.stringify(arr));
+}	
