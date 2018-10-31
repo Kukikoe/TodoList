b232cd45ed1700e7813827ee2ad032e9da00a4b9
Kukikoe <ja.bondareva2014@yandex.ru>
2018-10-31 15:22:47 +0200

    add todos in localStorage, added functions for delete, add, toggle, render todos

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,82 @@
 const addBtn = document.getElementById("add-btn");
 const addTask = document.getElementById("add-task");
 const addTaskBtn = document.getElementById("add-task-btn");
+const taskInput = document.querySelector(".add-task-input");
+const todoList = document.getElementById("todo-list");
+const deleteBtn = document.querySelectorAll(".icon-delete");
 
-//const deleteTodoBtn = document.querySelector("icon-delete");
-let taskInput = document.querySelector(".add-task-input");
+let arrayTodos = JSON.parse(localStorage.getItem("ArrayTodos")) || [];
+let _id = 0;
 
+renderTodo(arrayTodos, todoList);
 
-let newTodo;
-let arr = [];
-let boolValClick = false;
-
-addBtn.addEventListener("click", getBlockAddTask);
-addTaskBtn.addEventListener("click", function() {
-	let arrayTodos = JSON.parse(localStorage.getItem("ArrayTodos"));
-	console.log(arrayTodos.length)
-	let shit = document.querySelector("#todo-template").children[0];
-	let cln = shit.cloneNode(true);
-	addTaskInList(cln);
+addBtn.addEventListener("click", () => addTask.classList.toggle("active"));
+addTaskBtn.addEventListener("click", () => {
+	let todo = addTodo(taskInput.value, arrayTodos);
+	renderTodo([todo], todoList);
+	taskInput.value = "";
 });
 
-/*deleteTodoBtn.addEventListener("click", deleteTodo);
+todoList.addEventListener("click", (event) => {
+	let target = event.target;
+	let todo = target.closest('.todo');
 
-function deleteTodo() {
-	console.log("hef")
-}
-*/
-function getBlockAddTask() {
-	if (boolValClick) {
-		addTask.classList.remove("active");
-		boolValClick = false;
+	if (target.closest('.icon-delete')) {
+		deleteTodo(todo);
 	}
-	else {
-		addTask.classList.add("active");
-		boolValClick = true;
+	if (target.closest('.todo__checkbox') && target.tagName !== "INPUT") {
+		toggleTodo(todo);
 	}
+});
+
+function toggleTodo(elem) {
+	elem.classList.toggle("completed");
+	arrayTodos = arrayTodos.map((todo) => {
+		if (+elem.dataset.id == todo.id) {
+			todo.checked = !todo.checked;
+		}
+		return todo;
+	});
+	localStorage.setItem("ArrayTodos", JSON.stringify(arrayTodos));
 }
 
-function addTaskInList(cln) {
-	cln.querySelector(".todo__title").innerHTML = taskInput.value;
-	arr.push(cln.querySelector(".todo__title").innerHTML);
-	document.getElementById("todo-list").appendChild(cln);
-	localStorage.setItem("ArrayTodos",  JSON.stringify(arr));
+function deleteTodo(elem) {
+	arrayTodos = arrayTodos.filter((todo) => todo.id !== parseInt(elem.dataset.id));
+	localStorage.setItem("ArrayTodos", JSON.stringify(arrayTodos));
+	todoList.removeChild(elem);	
+}
+
+function getId() {
+	if (arrayTodos.length) {
+		_id = +arrayTodos[arrayTodos.length-1].id;
+	}
+	return ++_id;
+}
+
+function addTodo(title, arrayTodos) {
+	let todo = {
+		title: title,
+		checked: false,
+		id: getId()
+	};
+	arrayTodos.push(todo);
+	
+	localStorage.setItem("ArrayTodos", JSON.stringify(arrayTodos));
+	return todo;
 }	
+
+function renderTodo(array, elem) {
+	let template = document.querySelector("#todo-template").children[0];
+
+	for (let i = 0; i < array.length; i++) {
+		let cln = template.cloneNode(true);
+		cln.querySelector(".todo__title").innerHTML = array[i].title;
+		cln.querySelector(".todo__checkbox-input").checked = array[i].checked;
+		if (array[i].checked) {
+			cln.classList.add("completed");
+		}
+		cln.setAttribute("id", "todo-" + array[i].id);
+		cln.setAttribute("data-id", array[i].id);
+		elem.appendChild(cln);
+	}
+}
